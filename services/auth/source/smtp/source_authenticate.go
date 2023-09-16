4d9f4ed5cd5813f05ebcba6a9c7e561234f21112
hyg <hyg@webterren.com>
2023-09-16 10:32:57 +0800

    reword the comment

--- services/auth/source/smtp/source_authenticate.go
+++ services/auth/source/smtp/source_authenticate.go
@@ -43,7 +43,7 @@ func (source *Source) Authenticate(ctx context.Context, user *user_model.User, u
 	if err := Authenticate(auth, source); err != nil {
 		// when authentication via smtp fails, wraps ErrInvalidArgument
 		// with the original textproto.Error as the cause,
-		// so it will show username or password to the user
+		// so it will show username_password_incorrect to the user
 		// while log the original error so that admin can check.
 		// see: routers/web/auth/auth.go SiginPost
 		if tperr, ok := err.(*textproto.Error); ok {
